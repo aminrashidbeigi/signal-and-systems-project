6b009afc6db3427d10b46e6d43be660c4bfec90c
Amin Rashidbeigi <a.rashidbeigi@gmail.com>
2018-07-13 20:43:28 +0430

    Q2 completed

--- Q2/Q2_1.m
+++ Q2/Q2_1.m
@@ -1,18 +1,20 @@
-[signal, fs] = audioread('Pulse.wav');
-player = audioplayer(signal, fs);
-play(player);
+[audio, f] = audioread('Pulse.wav');
+player = audioplayer(audio, f);
+% play(player);
 
-low_filtered = filter(low, 1, signal);
-high_filtered = filter(high, 1, signal);
+low_filtered = filter(low, 1, audio);
+audiowrite('Pulse_lowpass.wav', low_filtered, f);
+high_filtered = filter(high, 1, audio);
+audiowrite('Pulse_highpass.wav', high_filtered, f);
 
 subplot(3,1,1);
-spectrogram(signal, fs);
-
+spectrogram(audio, f);
+title('Pulse signal');
 
 subplot(3,1,2);
-spectrogram(low_filtered, fs);
-
+spectrogram(low_filtered, f);
+title('low pass filtered Pulse signal');
 
 subplot(3,1,3);
-spectrogram(high_filtered, fs);
-
+spectrogram(high_filtered, f);
+title('high pass filtered Pulse signal');
